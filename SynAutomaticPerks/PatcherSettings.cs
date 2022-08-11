5fd85dbb111306c68b4ec743133c4f5389e4d9ba
TokcDK <TokcDK@Gmail.com>
2022-08-11 17:32:41 +0500

    added settings and helpers

---
+++ SynAutomaticPerks/PatcherSettings.cs
@@ -0,0 +1,11 @@
+﻿using Mutagen.Bethesda.Plugins;
+using Mutagen.Bethesda.Synthesis.Settings;
+using StringCompareSettings;
+using System.Collections.Generic;
+
+namespace SynAutomaticPerks
+{
+    public class PatcherSettings
+    {
+    }
+}
